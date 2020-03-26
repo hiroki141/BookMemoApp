8e7fde91f310d8b82b78c6028bc3dbe9f728d470
hiroki141 <ifuku.14ll@gmail.com>
2020-03-26 15:42:27 +0900

    箇条書き機能の追加

--- BookMemo/Controller/CreateViewController.swift
+++ BookMemo/Controller/CreateViewController.swift
@@ -21,6 +21,7 @@ class CreateViewController: UIViewController,UITextViewDelegate {
         didSet{
             memoTextView.layer.cornerRadius = 10.0
             memoTextView.layer.masksToBounds = true
+            memoTextView.font = UIFont.systemFont(ofSize: 16.0)
             memoTextView.delegate = self
         }
     }
@@ -31,7 +32,30 @@ class CreateViewController: UIViewController,UITextViewDelegate {
             memoTextView.resignFirstResponder()
         }
     }
+    var makeABullet = false
+    let bulletButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.size.width-105, y: 0, width: 50, height: 44))
     
+    @objc func onClickBulletButton(sender: UIButton){
+        if makeABullet == false{
+            makeABullet = true
+            bulletButton.tintColor = .blue
+            
+            let location = memoTextView.selectedRange.location
+            let previousOneCharacter = (memoTextView.text as NSString).substring(with: NSRange(location: location-1 , length: 1))
+            
+            // メモの先頭、または直前で改行しているところから始めるとき、そのまま点を打つ
+            if previousOneCharacter == "\n" || memoTextView.text == ""{
+                memoTextView.insertText(" \u{2022}  ")
+                
+            //　それ以外の時、改行してから点を打つ
+            }else{
+                memoTextView.insertText("\n \u{2022}  ")
+            }
+        }else{
+            makeABullet = false
+            bulletButton.tintColor = .white
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,17 +66,40 @@ class CreateViewController: UIViewController,UITextViewDelegate {
         publisherLabel.text = bookData.publisher
         
         //キーボードに完了ボタンを追加
-        let customBar = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 44))
+        
         let commitButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.size.width-50, y: 0, width: 50, height: 44))
         commitButton.setTitle("完了", for: .normal)
         commitButton.setTitleColor(UIColor.blue, for: .normal)
         commitButton.addTarget(self, action: #selector(EditMemoViewController.onClickCommitButton), for: .touchUpInside)
+        
+        let bulletImage = UIImage(named: "bullet.png")?.withRenderingMode(.alwaysTemplate)
+        bulletButton.setImage(bulletImage, for: .normal)
+        bulletButton.tintColor = .white
+        bulletButton.addTarget(self, action: #selector(EditMemoViewController.onClickBulletButton), for: .touchUpInside)
+        
+        let customBar = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 44))
+        customBar.backgroundColor = .systemGray2
         customBar.addSubview(commitButton)
+        customBar.addSubview(bulletButton)
+        
         memoTextView.inputAccessoryView = customBar
         memoTextView.keyboardType = .default
         
         saveButton.layer.cornerRadius = 10.0
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+           //　箇条書きを作成
+           if makeABullet == true{
+               if text == "\n"{
+                   memoTextView.insertText("\n \u{2022}  ")
+                   return false
+               }else{
+                   return true
+               }
+           }
+           return true
+       }
     
     @IBAction func saveAction(_ sender: Any) {
         //新しいデータの作成
