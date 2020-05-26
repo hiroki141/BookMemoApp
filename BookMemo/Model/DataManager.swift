b2bca9d42b7abd38160e9a6a7cc0ec14c7f01c9c
hiroki141 <ifuku.14ll@gmail.com>
2020-05-26 20:41:16 +0900

    context をdataManager classのinstanceに変更

--- BookMemo/Model/DataManager.swift
+++ BookMemo/Model/DataManager.swift
@@ -12,6 +12,7 @@ import Foundation
 
 class DataManager: NSObject {
     var persistentContainer: NSPersistentContainer!
+    let context: NSManagedObjectContext
 
     init(completionClosure: @escaping() -> Void) {
         persistentContainer = NSPersistentContainer(name: "BookMemo")
@@ -21,11 +22,11 @@ class DataManager: NSObject {
             }
             completionClosure()
         }
+        context = persistentContainer.viewContext
     }
 
     //新規作成
     func createBook() -> Book {
-        let context = persistentContainer.viewContext
         if let book: Book = NSEntityDescription.insertNewObject(forEntityName: "Book", into: context) as? Book {
             return book
         }else{
@@ -35,20 +36,18 @@ class DataManager: NSObject {
 
     //保存
     func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        print("saveContext() is called")
+        
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
     }
 
     //読み込み
     func getBooks() -> [Book] {
-        let context = persistentContainer.viewContext
         let booksfetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
         let sort = NSSortDescriptor(key: "editDate", ascending: false)
         booksfetch.sortDescriptors = [sort]
@@ -61,30 +60,29 @@ class DataManager: NSObject {
     }
 
     //検索
-    func serchBook(id: String) -> [Book] {
-        let context = persistentContainer.viewContext
+    func serchBook(id: String) -> Book {
         let booksfetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
         let predicate = NSPredicate(format: "id == %@", id)
         booksfetch.predicate = predicate
 
         do {
             let fetchedBooks = try context.fetch(booksfetch) as! [Book]
-            return fetchedBooks
+            let book = fetchedBooks[0]
+            return book
         } catch {
             fatalError("Failed to fetch books: \(error)")
         }
     }
 
-    //更新
-    func updateBook(id: String, updatedBook: Book) {
-        var Books = serchBook(id: id)
-        Books[0] = updatedBook
-        saveContext()
-    }
+//    //更新
+//    func updateBook(id: String, updatedBook: Book) {
+//        var Book = serchBook(id: id)
+//        Book = updatedBook
+//        saveContext()
+//    }
 
     //削除
     func delete(id: String) {
-        let context = persistentContainer.viewContext
         let booksfetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
         let predicate = NSPredicate(format: "id == %@", id)
         booksfetch.predicate = predicate
